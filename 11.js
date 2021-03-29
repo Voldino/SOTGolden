7043c550bd527f9d9b72f9ae5d94429714988a7f
DESKTOP-QM0CI15\maroc <marocchi.federico@gmail.com>
2021-03-29 21:54:41 +0200

    Publish new plugin

--- 11.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[11,12],{1869:function(n,o){},1870:function(n,o){}}]);
--- 12.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[12,11],{1869:function(n,o){},1870:function(n,o){}}]);
